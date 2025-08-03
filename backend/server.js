bc2fedf4b10c94813b3422258093b5ccff92e0e1
SHAHID-CODES-44 <shahidfirozaba@gmail.com>
2025-08-03 20:38:48 +0530

    Updated backend to use process.env.PORT for Railway

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import userRoutes from './routes/userRoutes.js';
 import lBoardRoutes from './routes/lBoardRoutes.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +22,5 @@ app.get('/', (req, res) => {
   res.send('Backend is running...');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
